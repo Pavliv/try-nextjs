80d10eb9074e18caec9a86636b537f697c292354
Marian <maryan.pavliv.com>
2024-11-21 14:53:39 +0200

    [init] add class to title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="p-5">
-      <h1>Welcome to - try Next.js</h1>
+      <h1 className="text-4xl font-bold">Welcome to - try Next.js</h1>
     </div>
   );
 }
